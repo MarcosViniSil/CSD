64675cee4d657c235ace8551320475454a723c28
Marcos Vinícius <121313831+MarcosViniSil@users.noreply.github.com>
2024-12-17 08:32:04 -0300

    Update teste_routh.m

--- teste_routh.m
+++ teste_routh.m
@@ -1,8 +1,15 @@
-coeficientes = [1,6,11,6];
-[estavel, polos_lado_direito,matriz_resultado] = routh(coeficientes);
-display("Sistema estável? ");
+coeficientes = [1,6,11,6,200] % SISTEMA INSTAVEL
+%coeficientes = [1,6,11,6] % SISTEMA ESTAVEL
+[estavel, polos_lado_direito,polos_lado_esquerdo,matriz_resultado] = routh(coeficientes);
+display("Sistema estável? (1) -> true, (0) -> false");
+
 disp(estavel);
+
 display("Polos semiplano direito: ");
 disp(polos_lado_direito);
+
+display("Polos semiplano esquerdo: ");
+disp(polos_lado_esquerdo);
+
 display("Matriz resultante: ");
 disp(matriz_resultado)
